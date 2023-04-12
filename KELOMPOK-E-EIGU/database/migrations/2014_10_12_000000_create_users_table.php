e9a6ef548472d9aa154eea3747d74b6ffa301211
nahiddm <ahmadnahid2904@gmaiil.com>
2023-04-12 12:38:50 +0700

    Revert "Revert "Revert "PBI_1_SignUp Reff#1"""
    This reverts commit 46681acc02348ae3c09a684f190afead591c38f8.

--- KELOMPOK-E-EIGU/database/migrations/2014_10_12_000000_create_users_table.php
+++ KELOMPOK-E-EIGU/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password')->nullable();
-            $table->string('motto')->nullable();
-            $table->string('region')->nullable();
-            $table->string('city')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
--- KELOMPOK-E-EIGU/routes/web.php
+++ KELOMPOK-E-EIGU/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/signup', [SignUpController::class, 'index']);
-Route::post('/signup', [SignUpController::class, 'signup']);
+Route::get('/', function () {
+    return view('welcome');
+});
